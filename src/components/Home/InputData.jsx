eb99362f183c078296c1bf8c4edb71defe0addc3
Santhoshkumar2305 <153523447+Santhoshkumar2305@users.noreply.github.com>
2025-02-07 00:38:41 +0530

    Update InputData.jsx

--- src/components/Home/InputData.jsx
+++ src/components/Home/InputData.jsx
@@ -22,7 +22,7 @@ const InputData = ({ InputDiv, setInputDiv, fetchTasks, UpdatedData, setUpdatedD
         }
         else {
             console.log(Data)
-            const response = await axios.post("http://localhost:3000/api/tasks/create-task", Data, { headers })
+            const response = await axios.post("https://task-manager-backend-8cyi.onrender.com/api/tasks/create-task", Data, { headers })
             if (response.data.message === "Task Created") {
                 setData({ title: "", desc: "", due: "" })
                 setInputDiv("hidden")
@@ -37,7 +37,7 @@ const InputData = ({ InputDiv, setInputDiv, fetchTasks, UpdatedData, setUpdatedD
             alert("All fields are required")
         }
         else {
-            const response = await axios.put(`http://localhost:3000/api/tasks/update-task/${UpdatedData.id}`, Data, { headers })
+            const response = await axios.put(`https://task-manager-backend-8cyi.onrender.com/api/tasks/update-task/${UpdatedData.id}`, Data, { headers })
             setUpdatedData({
                 id: "", title: "", desc: ""
             })
